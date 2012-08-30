3cd9910635b073fa4cff093b71006825c3a5e815
Olivier Guyollot <oguyollot.degetel@tf1.fr>
2012-08-30 18:26:29 +0200

    Fix fatal error and ignore generated files

--- Form/TranslationContentType.php
+++ Form/TranslationContentType.php
@@ -4,7 +4,7 @@ namespace Propel\TranslationBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormViewInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 
 /**
@@ -30,9 +30,9 @@ class TranslationContentType extends AbstractType
      * (non-PHPdoc)
      * @see Symfony\Component\Form.AbstractType::buildView()
      */
-    public function buildView(FormViewInterface $view, FormInterface $form, array $options)
+    public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        $view->setVar('label', $form['locale']->getData());
+        $view->set('label', $form['locale']->getData());
     }
 
     /**
